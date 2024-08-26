6d9400eb7baabdb5d887b0f3599d7eb9e7a4dc10
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-08-27 04:07:53 +0800

    feat: add leaderboard api

---
+++ src/app/(api)/leaderboard/route.ts
@@ -0,0 +1,60 @@
+import { dbClient } from '@/db'
+
+export async function GET() {
+  const data = await dbClient.query.tweets.findMany({
+    where(fields, operators) {
+      return operators.eq(fields.isPublic, true)
+    },
+    columns: {
+      tweetId: true,
+      tweetUserId: true,
+      tweetProfileImageUrl: true,
+      tweetUserName: true,
+      tweetUserScreenName: true,
+    },
+  })
+
+  const tweetCounts = Object.values(
+    data.reduce(
+      (
+        acc: Record<
+          string,
+          {
+            count: number
+            tweetUserId: string
+            tweetProfileImageUrl: string
+            tweetUserName: string
+            tweetUserScreenName: string
+          }
+        >,
+        tweet
+      ) => {
+        if (!acc[tweet.tweetUserId]) {
+          acc[tweet.tweetUserId] = {
+            count: 0,
+            tweetUserId: tweet.tweetUserId,
+            tweetProfileImageUrl: tweet.tweetProfileImageUrl,
+            tweetUserName: tweet.tweetUserName,
+            tweetUserScreenName: tweet.tweetUserScreenName,
+          }
+        }
+        acc[tweet.tweetUserId].count++
+        return acc
+      },
+      {}
+    )
+  )
+
+  // Sort by count (desc) and then by tweetUserScreenName (asc)
+  const sorted = tweetCounts.sort(
+    (a, b) => b.count - a.count || a.tweetUserScreenName.localeCompare(b.tweetUserScreenName)
+  )
+
+  // Add rank
+  const ranked = sorted.map((item, index) => ({
+    ...item,
+    rank: index + 1,
+  }))
+
+  return Response.json({ data: ranked })
+}
