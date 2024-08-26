b49eb4b5dfe56c249dd76abe685c5f8c97e4de22
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-08-27 04:15:38 +0800

    feat: Add API route for retrieving statistics

---
+++ src/app/(api)/stats/route.ts
@@ -0,0 +1,32 @@
+import { dbClient } from '@/db'
+
+export async function GET() {
+  const tweets = await dbClient.query.tweets.findMany({
+    where(fields, operators) {
+      return operators.eq(fields.isPublic, true)
+    },
+    columns: {
+      tweetId: true,
+      tweetUserId: true,
+    },
+  })
+
+  const usersSet = tweets.map((tweet) => tweet.tweetUserId)
+
+  const topics = await dbClient.query.topics.findMany({
+    where(fields, operators) {
+      return operators.eq(fields.isPublic, true)
+    },
+    columns: {
+      id: true,
+    },
+  })
+
+  return Response.json({
+    data: {
+      users: usersSet.length,
+      topics: topics.length,
+      tweets: tweets.length,
+    },
+  })
+}
