4d1c59dd248b4bd4092785ee6aa6e4f167a1e9a2
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-09-07 00:31:10 +0800

    feat: add random number as versioning check

---
+++ src/app/(api)/random/route.ts
@@ -0,0 +1,8 @@
+export const dynamic = 'force-dynamic'
+
+export async function GET(): Promise<Response> {
+  const randomNum = Math.floor(Math.random() * 1000) + 1
+  const time = new Date().toISOString()
+
+  return Response.json({ data: { random: randomNum, time } })
+}
